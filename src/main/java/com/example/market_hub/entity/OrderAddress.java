218b600e3b3867ecf978b74c98a3cbfc435cb5f6
nikita09icloud.com <krivcovnikita35@gmail.com>
2026-07-25 11:57:18 +0300

    Create entity
    Create OrderAddress

---
+++ src/main/java/com/example/market_hub/entity/OrderAddress.java
@@ -0,0 +1,34 @@
+package com.example.market_hub.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "order_addresses")
+public class OrderAddress {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "country", nullable = false)
+    private String country;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "street", nullable = false)
+    private String street;
+
+    @Column(name = "house", nullable = false)
+    private String house;
+
+    @Column(name = "apartment", nullable = false)
+    private String apartment;
+
+    @OneToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
