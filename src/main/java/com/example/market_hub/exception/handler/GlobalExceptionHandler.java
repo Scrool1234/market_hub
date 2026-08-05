36f0138d0e2b51e9269179d293375f13e9066651
nikita09icloud.com <krivcovnikita35@gmail.com>
2026-08-05 22:36:26 +0300

    Implement exceptions management
    Add DuplicateResourceException, ResourceNotFoundException and GlobalExceptionHandler

---
+++ src/main/java/com/example/market_hub/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.example.market_hub.exception.handler;
+
+import com.example.market_hub.exception.dto.ErrorDTO;
+import com.example.market_hub.exception.DuplicateResourceException;
+import com.example.market_hub.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDTO> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                ErrorDTO.builder()
+                        .status(HttpStatus.NOT_FOUND.value())
+                        .error(HttpStatus.NOT_FOUND.getReasonPhrase()).message(ex.getMessage()).build()
+        );
+    }
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<ErrorDTO> handleDuplicateResourceException(DuplicateResourceException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                ErrorDTO.builder()
+                        .status(HttpStatus.CONFLICT.value())
+                        .error(HttpStatus.CONFLICT.getReasonPhrase()).message(ex.getMessage()).build()
+        );
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorDTO> handleBadCredentialsException(BadCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+                ErrorDTO.builder()
+                        .status(HttpStatus.UNAUTHORIZED.value())
+                        .error(HttpStatus.UNAUTHORIZED.getReasonPhrase()).message(ex.getMessage()).build()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ErrorDTO.builder()
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .error(HttpStatus.BAD_REQUEST.getReasonPhrase()).message(ex.getMessage()).build()
+        );
+    }
+}
--- src/main/java/com/example/market_hub/handler/GlobalExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.example.market_hub.handler;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-
-}
